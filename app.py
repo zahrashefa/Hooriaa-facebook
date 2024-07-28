21c94a7ebc5f6dfb54530bd8189b513218bd0431
zahrashefa <zahrashefa229@gmail.com>
2024-07-28 23:28:04 +0000

    I have added html templates and static folder .

--- app.py
+++ app.py
@@ -1,7 +1,18 @@
-from flask import Flask
-app=Flask(__name__)
+from flask import Flask, render_template , send_from_directory
+
+app = Flask(__name__)
+
+students=[{"name":"zahra", "age":20 , "city":"New York"}, {"name":"Hussain", "age":22 , "city":"New York"}]
+
+
 @app.route("/")
 def hello():
-  return("hello world")
-if __name__=="__main__":
-  app.run(host="0.0.0.0" , debug=True)
+  return render_template('home.html' , st=students)
+
+@app.route('/static/<path:path>')
+def serve_static(path):
+    return send_from_directory('static', path)
+
+
+if __name__ == "__main__":
+  app.run(host="0.0.0.0", debug=True)
